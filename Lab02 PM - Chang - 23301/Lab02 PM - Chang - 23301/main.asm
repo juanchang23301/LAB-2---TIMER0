0d6f48d8dedd7cd936f4fdb3a9daca9077a01ce1
juanchang23301 <cha23301@uvg.edu.gt>
2025-02-12 15:05:36 -0600

    Create main.asm

---
+++ Lab02 PM - Chang - 23301/Lab02 PM - Chang - 23301/main.asm
@@ -0,0 +1,15 @@
+;************************************************************************
+; Universidad del Valle de Guatemala
+; IE2023: Programación de Microcontroladores
+; Test.asm
+;
+; Author: Juan Chang
+; Proyecto: Ejemplo
+; Hardware: ATMega328P
+; Creado: 2/5/2025 1:54:05 PM
+; Modificado: 30/11/2024
+; Descipción: Este programa consiste ens
+;************************************************************************
+
+.include "M328PDEF.inc"
+.org 0x0000
