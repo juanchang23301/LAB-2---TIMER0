60605e53a74181db9f52e8045cfa54dc2cc7e153
juanchang23301 <cha23301@uvg.edu.gt>
2025-02-13 10:55:33 -0600

    Update main.asm

--- Lab02 PM - Chang - 23301/Lab02 PM - Chang - 23301/main.asm
+++ Lab02 PM - Chang - 23301/Lab02 PM - Chang - 23301/main.asm
@@ -10,6 +10,36 @@
 ; Modificado: 30/11/2024
 ; Descipción: Este programa consiste ens
 ;************************************************************************
-
 .include "M328PDEF.inc"
 .org 0x0000
+
+
+LDI		R16, 0xFF
+OUT		DDRC, R16
+
+LDI		R16, 0x05
+OUT		TCCR0B, R16		; Configurar Timer0 con prescaler de 1024
+
+MAIN_LOOP:
+	CALL	T100ms		; Se necesitan 6 desbordamientos (~100 ms)
+	INC		R17
+	OUT		PORTC, R17
+	RJMP	MAIN_LOOP
+
+T100ms:
+	LDI		R18, 10
+
+TIMER: 
+	SBIS	TIFR0, TOV0			; Esperar desbordamiento
+	RJMP	TIMER
+	LDI		R16, (1 << TOV0)	; Limpiar la bandera de desbordamiento
+	OUT		TIFR0, R16
+	DEC		R18
+	BRNE	TIMER
+	RET
+
+
+
+
+
+
