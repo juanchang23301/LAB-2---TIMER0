223f2f7e7d145c17582280bedf46dee60079b336
juanchang23301 <cha23301@uvg.edu.gt>
2025-02-18 16:59:21 -0600

    Update main.asm

--- Lab02 PM - Chang - 23301/Lab02 PM - Chang - 23301/main.asm
+++ Lab02 PM - Chang - 23301/Lab02 PM - Chang - 23301/main.asm
@@ -82,43 +82,91 @@ START:
 	LDI	R16, 0b01110001		;F en el display
 	ST Z+, R16
 
-	LDI R16, 0X00
-	OUT PORTD, R16			; Iniciar el display apagado
-
 	LDI ZH, 0X01			; Cargar parte alta de la tabla
     LDI ZL, 0X00			; Cargar parte baja de la tabla
 
+	LD R16, Z				; Revisamos el siguiente registro del puntero
+	OUT PORTD, R16			; Iniciar el display en 0
+
+	LDI R20, 0x00			; Revisamos el siguiente registro del puntero
+
 MAIN_LOOP:
-	CALL	T100ms			; Se necesitan 10 desbordamientos (~100 ms)
-	INC		R17
-	OUT		PORTC, R17
+    RCALL   T100ms          ; Se necesitan 10 desbordamientos (~100 ms)
+    INC     R17
+    ANDI    R17, 0x0F       ; Mantiene el contador en 4 bits (0-15)
+    
+    IN      R16, PORTC      ; Lee el estado actual de PORTC
+    ANDI    R16, 0x10       ; Encender solo PC4  bit 4 (LED)
+    MOV     R22, R17        ; Copia R17 a R22
+    ANDI    R22, 0x0F       ; Mantiene solo los 4 bits menos significativos
+    OR      R16, R22        ; Combina el estado del LED con el contador
+    OUT     PORTC, R16      ; Actualiza PORTC
+    
+    RCALL   CHECK_COUNT     ; Verifica si los contadores coinciden
+    RCALL   CHECK_INC		; Verifica los botones
+    RJMP    MAIN_LOOP
 
-	SBIC PINB, 0			 ; Si PB0 (incrementar) está en alto, salta
-    RJMP CHECK_DEC			 ; Si no, verifica el botón de decremento
-    RCALL DELAY				 ; Antirrebote 
+CHECK_INC:
+	SBIC PINB, 0			; Si PB0 (incrementar) está en alto, salta
+    RJMP CHECK_DEC			; Si no, verifica el botón de decremento
+    RCALL DELAY				; Antirrebote 
+ 
+	INC R20					; Incrementa el contador
+    ANDI R20, 0x0F			; Mantiene el contador en 4 bits (0-15)
 
-	ANDI ZL, 0x0F			 ; Mantener valores entre 0-F automáticamente
-	LD R16, Z+				 ; Revisamos el siguiente registro del puntero
-	OUT PORTD, R16			 ; Muestra el valor del contador en el display 
-	RCALL WAIT_FOR_RELEASE	 ; Revisar que no hayan pushbuttons presionados
+	LDI ZH, 0x01			; Cargar parte alta de la tabla
+    LDI ZL, 0x00			; Cargar parte baja de la tabla
+    MOV R16, R20			; Copiar el contador a R16
+    ADD ZL, R16				; Sumar el offset al puntero
+    LD R16, Z				; Cargar el valor correspondiente
+    OUT PORTD, R16			; Mostrar en el display
+
+	RCALL WAIT_FOR_RELEASE	; Revisar que no hayan pushbuttons presionados
+	RJMP MAIN_LOOP
 
 CHECK_DEC:
-	SBIC PINB, 1			 ; Si PB1 (decrementar) está en alto, salta
-    RJMP MAIN_LOOP			 ; Si no, regresar al main
-    RCALL DELAY				 ; Antirrebote 
+	SBIC PINB, 1			; Si PB1 (decrementar) está en bajo
+	RET						; Si no, regresa
+    RCALL DELAY             ; Antirrebote
+    
+    DEC R20                 ; Decrementar el contador
+    ANDI R20, 0x0F			; Mantiene el contador en 4 bits (0-15)
+    
+    LDI ZH, 0x01			; Cargar parte alta de la tabla
+    LDI ZL, 0x00			; Cargar parte baja de la tabla
+    MOV R16, R20			; Copiar el contador a R16
+    ADD ZL, R16				; Sumar el offset al puntero
+    LD R16, Z				; Cargar el valor correspondiente
+    OUT PORTD, R16			; Mostrar en el display
+	
+	RCALL WAIT_FOR_RELEASE	; Revisar que no hayan pushbuttons presionados
+	RJMP MAIN_LOOP
 
-    CPI ZL, 0x00			 ; Verificar si estamos en 0
-    BRNE DEC_NORMAL			 ; Si no está en 0, hacer decremento normal
-    LDI ZL, 0x10			 ; Si está en 0, ponerlo en 0x10 (para que al restar pase a 0x0F)
+CHECK_COUNT:
+    CP      R17, R20        ; Compara R17 (contador segundos) con R20 (contador botones)
+    BRNE    RETURN_CHECK    ; Si no son iguales, retorna
+    
+    CLR     R17             ; Reinicia el contador de segundos
+    SBRS    R21, 0          ; Salta si R21 bit 0 es 1 (LED encendido)
+    RJMP    LED_ON			; Si el LED está apagado, enciéndelo
+    RJMP    LED_OFF			; Si el LED está encendido, apágalo
 
-DEC_NORMAL:
-    LD R16, -Z				 ; Revisamos el siguiente registro del puntero
-    OUT PORTD, R16           ; Muestra el valor del contador en el display   
-    RCALL WAIT_FOR_RELEASE	 ; Revisar que no hayan pushbuttons presionados
-    RJMP MAIN_LOOP
+LED_ON:
+    SBI     PORTC, 4        ; Enciende LED en PC4
+    LDI     R16, 1
+    MOV     R21, R16        ; Marca LED como encendido
+    RET
+	
+LED_OFF:
+    CBI     PORTC, 4        ; Apaga LED en PC4
+    CLR     R21             ; Marca LED como apagado
+    RET
+
+RETURN_CHECK:
+    RET
 
 T100ms:
-	LDI		R18, 10
+	LDI		R18, 15
 TIMER: 
 	SBIS	TIFR0, TOV0			; Esperar desbordamiento
 	RJMP	TIMER
@@ -134,14 +182,7 @@ WAIT_FOR_RELEASE:
     RJMP WAIT_FOR_RELEASE
 	SBIS PINB, 1	
 	RJMP WAIT_FOR_RELEASE
-    SBIS PINB, 2
-	RJMP WAIT_FOR_RELEASE
-    SBIS PINB, 3
-    RJMP WAIT_FOR_RELEASE
-	SBIS PINB, 4
-    RJMP WAIT_FOR_RELEASE
     RET
-
 
 	;Antirrebote
 DELAY:
@@ -156,10 +197,3 @@ SUB_DELAY2:
     CPI     R18, 0
     BRNE    SUB_DELAY2
     RET
-
-
-
-
-
-
-
